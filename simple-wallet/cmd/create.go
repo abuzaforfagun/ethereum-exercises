286398a57ca147bd3fd70aaeca2c8c4cbacbc09c
Fagun <mdabuzaforfagun@gmail.com>
2024-11-01 14:09:49 +0600

    create new wallet

--- simple-wallet/cmd/create.go
+++ simple-wallet/cmd/create.go
@@ -1,11 +1,35 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/spf13/cobra"
+)
 
 var createWalletCmd = &cobra.Command{
-	Use:   "create [wallet name]",
-	Args:  cobra.ExactArgs(1),
+	Use:   "create [name]",
 	Short: "create a new wallet",
 	Long:  "create a new wallet and return the wallet address",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Error 'name' argument is required")
+			cmd.Help()
+			os.Exit(1)
+		}
+		walletName := args[0]
+		address := createWallet(walletName)
+		fmt.Println("Account created! Address: ", address)
+	},
+}
+
+func createWallet(name string) string {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		fmt.Println("Unable to generate the key")
+	}
+
+	address := crypto.PubkeyToAddress(privateKey.PublicKey)
+	return address.Hex()
 }
