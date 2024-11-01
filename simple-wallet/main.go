5d91a8c0f271fcf3430bcc28dda8eb7e8777bcc1
Fagun <mdabuzaforfagun@gmail.com>
2024-11-01 16:44:21 +0600

    add random secret key when it is not there

--- simple-wallet/main.go
+++ simple-wallet/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"database/sql"
+	"encoding/hex"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/abuzaforfagun/ethereum-exercises/simple-wallet/cmd"
 	_ "github.com/mattn/go-sqlite3"
@@ -11,10 +15,25 @@ import (
 
 func main() {
 	db := initDatabase()
+	verifySecretKey()
 	defer db.Close()
 	cmd.Execute(db)
 }
 
+func verifySecretKey() {
+	secretKey := os.Getenv("wallet_secret_key")
+	if secretKey == "" {
+		key := make([]byte, 16)
+
+		_, err := rand.Read(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hexKey := hex.EncodeToString(key)
+		os.Setenv("wallet_secret_key", string(hexKey))
+		fmt.Printf("A random secret is added to the enviornment\n", key)
+	}
+}
 func initDatabase() *sql.DB {
 	db, err := sql.Open("sqlite3", "./wallet.db")
 	if err != nil {
